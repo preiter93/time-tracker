18623ef9453faa495ae8dfed7a59b4c7241f2352
Philipp Reiter <philipp.reiter@freiheit.com>
2023-10-08 22:21:58 +0200

    Initial setup gh pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,13 @@
 import adapter from '@sveltejs/adapter-static';
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter(),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/time-tracker-svelte' : '',
+			base: dev ? '' : process.env.BASE_PATH,
 		}
 	}
 };
