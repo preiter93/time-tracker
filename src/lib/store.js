14214c08df6a02c1e7005dc9bfa583609978f336
Philipp Reiter <philipp.reiter@freiheit.com>
2023-10-09 19:05:45 +0200

    Increase item id length

--- src/lib/store.js
+++ src/lib/store.js
@@ -154,7 +154,7 @@ class TimerItemValue {
 function generateRandomID() {
 	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 	let result = '';
-	for (let i = 0; i < 4; i++) {
+	for (let i = 0; i < 8; i++) {
 		const randomIndex = Math.floor(Math.random() * characters.length);
 		result += characters.charAt(randomIndex);
 	}
