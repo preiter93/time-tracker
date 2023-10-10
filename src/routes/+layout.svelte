c7ca8c53ae63a42e3c730ef394b95a7070a74604
Philipp Reiter <philipp.reiter@freiheit.com>
2023-10-10 02:00:34 +0200

    Update purple

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,6 @@
+<script>
+</script>
+
 <slot />
 <svelte:head>
 	<style>
@@ -11,6 +14,14 @@
 </svelte:head>
 
 <style>
+	:root {
+		--dark-purple: #691c52;
+		--light-purple: #913175;
+		--light-gray: #e9e8e8;
+		--dark-gray: #20262e;
+		--action-red: #c30101;
+		--turquoise: #1f7a8c;
+	}
 	:global(body) {
 		background-color: var(--bg-primary);
 		margin: 0;
@@ -22,24 +33,24 @@
 	}
 
 	:global(.light) {
-		--bg-header: #1f7a8c;
-		--text-header: #fff;
-		--bg-primary: #fff;
-		--bg-secondary: #000;
-		--text-primary: #000;
-		--btn-primary: #000;
-		--btn-secondary: #fff;
-		--btn-action: #c30101;
+		--bg-header: var(--turquoise);
+		--text-header: white;
+		--bg-primary: white;
+		--bg-secondary: black;
+		--text-primary: black;
+		--btn-primary: black;
+		--btn-secondary: white;
+		--btn-action: var(--action-red);
 	}
 
 	:global(.dark) {
-		--bg-header: #000;
-		--text-header: #913175;
-		--bg-primary: #20262e;
-		--bg-secondary: #913175;
-		--text-primary: #e9e8e8;
-		--btn-primary: #e9e8e8;
-		--btn-secondary: #913175;
-		--btn-action: #e9e8e8;
+		--bg-header: black;
+		--text-header: var(--dark-purple);
+		--bg-primary: var(--dark-gray);
+		--bg-secondary: var(--dark-purple);
+		--text-primary: var(--light-gray);
+		--btn-primary: var(--light-gray);
+		--btn-secondary: var(--dark-purple);
+		--btn-action: var(--light-gray);
 	}
 </style>
